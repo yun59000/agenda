c3619fb1605eb57979d393a11510d258e0ae2370
unknown <richard.yves@gmail.com>
2021-09-30 17:45:31 +0200

    add del, edit, search

--- test/test.py
+++ test/test.py
@@ -83,7 +83,8 @@ class MyAgendaEvent:
     lastIndex = self.getLastRecordIndex()
     index = str(int(lastIndex) + 1)
     y = { "event" + index :{
-          "lib" : self.lib + index ,
+          "lib" : self.lib,
+          "index" : index,
           "description" : self.description,
           "date_ori" : self.creationDateStr,
           "date_edit" : ""}
@@ -99,32 +100,55 @@ class MyAgendaEvent:
     # print("y: "+ str(y))
     return y
 
-  def editEvent(self, lib, key, value):
-    with open(self.filename,'r') as file:
-        # First we load existing data into a dict.
-        file_data = json.load(file)
-    for elmt in file_data:
-      values_view = elmt.keys()
-      value_iterator = iter(values_view)
-      first_key = next(value_iterator)
-      # print(first_key)
-      
-      if elmt[first_key]["lib"] == lib:
-        # print("trouvé")
-        # print("OldKey: "+key+" OldValue: "+elmt[first_key][key])
-        # print("a changer key :"+key + " value: "+value)
-        elmt[first_key][key] = value
-        # write_json(file_data)
-        with open(self.filename,'r+') as file:        
-          # Sets file's current position at offset.
-          file.seek(0)
-          # convert back to json.
-          json.dump(file_data, file, indent = 4, separators=(',',': '))
-          print('Successfully edit the JSON file')
-      # for k, v in elmt[first_key].items():
-      #   print("keys: "+str(k)+ " value: "+str(v))
+def editEvent(lib, key, value):
+  with open(filename,'r') as file:
+      # First we load existing data into a dict.
+      file_data = json.load(file)
+  for elmt in file_data:
+    values_view = elmt.keys()
+    value_iterator = iter(values_view)
+    first_key = next(value_iterator)
+    # print(first_key)
+    
+    if elmt[first_key]["lib"] == lib:
+      # print("trouvé")
+      # print("OldKey: "+key+" OldValue: "+elmt[first_key][key])
+      # print("a changer key :"+key + " value: "+value)
+      elmt[first_key][key] = value
+      # write_json(file_data)
+      with open(filename,'r+') as file:        
+        # Sets file's current position at offset.
+        file.seek(0)
+        # convert back to json.
+        json.dump(file_data, file, indent = 4, separators=(',',': '))
+        print('Successfully edit the JSON file')
 
-    # if file_data["lib"] == value:
+def delEvent(lib):
+  with open(filename,'r') as file:
+      # First we load existing data into a dict.
+      file_data = json.load(file)
+  newFileData = []
+  for elmt in file_data:
+    values_view = elmt.keys()
+    value_iterator = iter(values_view)
+    first_key = next(value_iterator)
+    # print(first_key)
+    
+    if elmt[first_key]["lib"] == lib:
+      pass      
+    else:
+      newFileData.append(elmt)
+  
+  valid = input("Etes vous sûre de vouloir supprimer l'event "+ lib + " ? y or n : ")
+  if valid == "y":          
+    with open(filename,'w') as file:        
+      # Sets file's current position at offset.
+      file.seek(0)
+      # convert back to json.
+      json.dump(newFileData, file, indent = 4)
+      print('Successfully del the event')
+  else:
+    choice()
 
 #**************************
 # # function to add to JSON
@@ -146,13 +170,13 @@ def create():
   err = True
   while err == True:
     evtStartDateYYYY = input("Date de début de l'evenement - Année: AAAA: ") #check date a mettre en place
-    if evtStartDateYYYY > 2020:
+    if int(evtStartDateYYYY) > 2020:
       err = False
     evtStartDateMM = input("Date de début de l'evenement - Mois: MM: ")
-    if evtStartDateMM > 0 and evtStartDateMM < 13:
+    if int(evtStartDateMM) > 0 and int(evtStartDateMM) < 13:
       err = False
     evtStartDateDD = input("Date de début de l'evenement - Jours: DD: ")
-    if evtStartDateDD > 0 and evtStartDateDD < 32:
+    if int(evtStartDateDD) > 0 and int(evtStartDateDD) < 32:
       err = False
     evtStartDate = evtStartDateYYYY + "-" + evtStartDateMM + "-" + evtStartDateDD
     lib = input("Nom de l'evenement: ")
@@ -170,17 +194,16 @@ def create():
   new_data = mon_evet.createEvent()
   if mon_evet.new == True:
     write_json_add(new_data)  
-    # pass
   else:
     print("event already exist please change lib")
+  choice()
   
 def days_between(d1, d2):
     d1 = datetime.datetime.strptime(d1, "%Y-%m-%d")
     d2 = datetime.datetime.strptime(d2, "%Y-%m-%d")
     return str(abs((d2 - d1).days))
-#print(days_between("2022-12-24","2021-12-24"))
 
-def showEvents(filename='../data/event.json'):
+def showEvents(filename='../data/event.json', notify=False):
     #lire    
     aujourdhui = datetime.datetime.now()
     timestampStr = aujourdhui.strftime("%Y-%m-%d")
@@ -206,57 +229,128 @@ def showEvents(filename='../data/event.json'):
             if eachItem[0][7:] == "milestone":
               date_time_obj = eachItem[1]
               diffDay = days_between(timestampStr,date_time_obj)
-              # print(eachItem[0]+" -> "+eachItem[1]+" ->prévu dans "+diffDay+" jours")
-              # print("diff : "+diffDay)
+              
               if int(diffDay) <= 15:
                 toNotify = True
                 listTemp.append(Fore.RED +eachItem[0]+" -> "+eachItem[1]+" ->prévu dans "+diffDay+" jours")
                 listOfData.append(eachItem[0]+" -> "+eachItem[1]+" ->prévu dans "+diffDay+" jours")
               elif int(diffDay) > 15 and int(diffDay) <= 31:
+                toNotify = True
                 listTemp.append(Fore.YELLOW +eachItem[0]+" -> "+eachItem[1]+" ->prévu dans "+diffDay+" jours")
                 listOfData.append(eachItem[0]+" -> "+eachItem[1]+" ->prévu dans "+diffDay+" jours")
-              else:
-                listOfData.append(eachItem[0]+" -> "+eachItem[1]+" ->prévu dans "+diffDay+" jours")
+              else:                
                 listTemp.append(Fore.GREEN +eachItem[0]+" -> "+eachItem[1]+" ->prévu dans "+diffDay+" jours")
-            else:
-              # print("%s -> %s" % (eachItem))
+                listOfData.append(eachItem[0]+" -> "+eachItem[1]+" ->prévu dans "+diffDay+" jours")
+            else:              
               listOfData.append("%s -> %s" % (eachItem))
               listTemp.append("%s -> %s" % (eachItem))
           if toNotify == True:
             listOfDataToNotify.append(listTemp)
-            for lignes in listTemp:
+
+          if notify == True:
+            for lignes in listOfDataToNotify:
+              for eachLigne in lignes:
+                print(eachLigne)
+              print("**************************")
+              input("Press Enter notif")
+              
+          else:
+            for lignes in listOfData:
               print(lignes)
-            input("Press a key to continue...")
-            
-        # print(elmt[first_key]["description"])
-        # print(elmt[first_key]["date_ori"])
-        # print(elmt[first_key]["date_edit"])
-          # print(list(elmt[first_key])[i])
-          print("**************************")
+            print("**************************")  
+            input("Press Enter")
+          
           i = i + 1
-    # sendMail(listOfData)
-    #afficher
-    
+    choice()
+    # sendMail(listOfData)    
+ 
+def searchEvent(searchLib):
+  with open(filename,'r') as file:
+        # First we load existing data into a dict.
+        file_data = json.load(file)
+  aujourdhui = datetime.datetime.now()
+  timestampStr = aujourdhui.strftime("%Y-%m-%d")
+  for files in file_data:
+    search = False
+    for eachfile in files.values():
+      # print("********search**********")
+      # print(eachfile)
+      for k, v in eachfile.items():
+        if v == searchLib:
+          search = True
+          break
+      listTemp = []
+      if search == True:
+        print("**************************")        
+        for eachdata in eachfile.items():
+          # print(eachdata[0]+" -> "+eachdata[1])
+          #***************
+          if eachdata[0][7:] == "milestone":
+            date_time_obj = eachdata[1]
+            diffDay = days_between(timestampStr,date_time_obj)
+            
+            if int(diffDay) <= 15:              
+              listTemp.append(Fore.RED +eachdata[0]+" -> "+eachdata[1]+" ->prévu dans "+diffDay+" jours")              
+            elif int(diffDay) > 15 and int(diffDay) <= 31:              
+              listTemp.append(Fore.YELLOW +eachdata[0]+" -> "+eachdata[1]+" ->prévu dans "+diffDay+" jours")              
+            else:                
+              listTemp.append(Fore.GREEN +eachdata[0]+" -> "+eachdata[1]+" ->prévu dans "+diffDay+" jours")              
+          else:                          
+            listTemp.append("%s -> %s" % (eachdata))
 
-# mon_evet.editEvent("nouvel event4","description","une autre edit descrp")
+      for allelmt in listTemp:
+        print(allelmt)
+  input("Press Enter")
+  choice()
 
-#controle unicité json lib                                 *******************OK
-#transform function def milestone to really add milestone to a date **********OK with obj 
-#add def to add milestone to the JSON Object               *******************OK
-#edit one param at a time                                  *******************OK
-print("Programme de gestion d'evenements calendaire")
-print("Actions : ")
-print(" 1 - Créer un évenement")
-print(" 2 - Editer un évenement")
-print(" 3 - Supprimer un évenement")
-print(" 4 - Consulter les événements")
+def choice():  
+  choice = False
+  while choice == False:
+    print("Programme de gestion d'evenements calendaire")
+    print("Actions : ")
+    print(" 1 - Créer un évenement")
+    print(" 2 - Editer un évenement")
+    print(" 3 - Supprimer un évenement")
+    print(" 4 - Consulter les événements")
+    print(" 5 - Consulter les événement à notifier")
+    print(" 6 - Rechercher un événement")
+    print(" 0 - exit")
+    x = input('Entrer une valeur : ')
+    if x == "1":
+      create()
+      choice = True
+    elif x == "2":
+      lib = input("enter lib: veille de noel17 ")
+      print('key: "lib", "description", "date_ori", "date_edit", "date_1_milestone", "date_2_milestone", "date_3_milestone", "date_4_milestone"')
+      key = input("enter the key to modify: ")
+      print("format de date: YYYY-MM-DD")
+      value = input("enter the new value: ")
+      editEvent(lib, key, value)
+      choice = True 
+    elif x == "3":
+      lib = input("enter lib: veille de noel17 ")
+      delEvent(lib)
+      choice = True
+    elif x == "4":
+      showEvents()
+      choice = True
+    elif x == "5":
+      showEvents(notify=True)
+      choice = True      
+    elif x == "6":
+      searchLib = input("enter lib: veille de noel17 ")
+      searchEvent(searchLib)
+      choice = True
+    elif x == "0":
+      print("Merci à bientôt")
+      exit()
+    else:
+      print("WIP")
+      input("Press Enter")
 
-x = input('Entrer une valeur : ')
-# print('You entered : ', x)
-def choice(i):  
-  if i == "1":
-    create()
-  elif i == "4":
-    showEvents()
+choice()
 
-choice(x)
+# ToDo
+# agenda
+    # events
+        # milestones
